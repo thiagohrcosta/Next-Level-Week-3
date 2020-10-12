c96f08d68075cc299012fbef0785de3a760c6b20
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-10-12 15:53:03 -0300

    Finalizando Aula 01

--- web/src/pages/OprhanagesMap.tsx
+++ web/src/pages/OprhanagesMap.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
+import {Map, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
@@ -22,7 +24,14 @@ function OrphanagesMap(){
         </footer>
       </aside>
 
-      <div></div>
+      <Map
+        center={[-19.9216771,-43.9531824]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
       
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
