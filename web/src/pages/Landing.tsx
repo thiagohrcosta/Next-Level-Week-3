72d580772bd9ea3d019e4b3cb583ad2dced574a3
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-10-12 15:16:44 -0300

    Iniciando criação de rotas com React Router Dom

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {FiArrowRight} from 'react-icons/fi';
+
+import '../styles/global.css';
+import '../styles/pages/landing.css';
+
+import logoImg from '../images/Logo.svg';
+
+
+function Landing(){
+  return(
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Belo Horizonte</strong>
+          <span>Minas Gerais</span>
+        </div>
+
+        <a href="http://" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
