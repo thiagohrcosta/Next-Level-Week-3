2a00b4babdf817d25c52a17312c3b622dab74c38
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-10-16 15:36:22 -0300

    Alterando mapa para usar MAPBOX

--- web/src/pages/OprhanagesMap.tsx
+++ web/src/pages/OprhanagesMap.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import {Map, TileLayer} from 'react-leaflet';
+import {Map, TileLayer, Marker} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+
+import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29,68],
+});
 
 function OrphanagesMap(){
   return(
@@ -29,8 +37,22 @@ function OrphanagesMap(){
         zoom={15}
         style={{ width: '100%', height: '100%' }}
       >
-        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {/*
+          <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      
+        */}
+
+        <TileLayer 
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+
+      <Marker 
+        icon={mapIcon}
+        position={[-19.9216771,-43.9531824]}
+      />
+      
       </Map>
+
 
       
       <Link to="" className="create-orphanage">
