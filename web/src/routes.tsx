d1fa99ecd5c43db883eccf5ddd613e37a83bfaf6
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-10-16 16:28:05 -0300

    Refatorando código

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Landing from './pages/Landing';
-import OrphanagesMap from './pages/OprhanagesMap';
+import OrphanagesMap from './pages/OrphanagesMap';
 
 import Orphanage from './pages/Orphanage';
 import CreateOrphanage from './pages/CreateOrphanage';
