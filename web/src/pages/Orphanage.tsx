8291d11757408c4dc57f601b1bceb58fdcfb3118
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-10-18 12:39:43 -0300

    Usando useState para alterar imagem

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -31,6 +31,7 @@ interface OrphanageParams{
 export default function Orphanage() {
   const params = useParams<OrphanageParams>();
   const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
 
   useEffect(() => {
     api.get(`orphanages/${params.id}`).then(response => {
@@ -48,12 +49,19 @@ export default function Orphanage() {
 
       <main>
         <div className="orphanage-details">
-          <img src={orphanage.images[0].url} alt={orphanage.name} />
+          <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
 
           <div className="images">
-            {orphanage.images.map(image => {
+            {orphanage.images.map((image, index) => {
               return(
-                <button key={image.id} className="active" type="button">
+                <button 
+                  key={image.id} 
+                  className="active" 
+                  type="button"
+                  onClick={() => {
+                    setActiveImageIndex(index)
+                  }}  
+                >
                   <img src={image.url} alt={orphanage.name} />
                 </button>
 
@@ -86,7 +94,10 @@ export default function Orphanage() {
               </Map>
 
               <footer>
-                <a href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
+                <a 
+                  target="_blank"
+                  rel="noopener noreferrer"  
+                  href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}>Ver rotas no Google Maps</a>
               </footer>
             </div>
 
